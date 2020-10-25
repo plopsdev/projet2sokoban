12f5197ce716f26740d8308bacc5093116674542
plopsdev <jonathan.miel@outlook.com>
2020-10-25 13:00:47 +0100

    first commit

---
+++ sokoban.py
@@ -0,0 +1,19 @@
+import time
+import sys
+import search
+from search import *
+from copy import deepcopy
+
+class Sokoban(Problem):
+    def __init__(self):
+        #todo
+    def actions(self, state):
+        #todo
+    def result(self, state, action):
+        #todo
+    def goal_test(self, state):
+        #todo
+
+class State:
+    def __init__(self):
+        #todo
