1f2998760e7844dae6de1d9d7804ae966f35f0af
plopsdev <jonathan.miel@outlook.com>
2020-10-28 16:35:55 +0100

    fix variables

--- sokoban.py
+++ sokoban.py
@@ -8,15 +8,20 @@ class State:
     def __init__(self, grid, curr_pos, goal_pos, curr_ok):
         self.grid=grid
         self.curr_pos=curr_pos #position of the @
-        self.goal_pos=goal_pos #array of the position(s) of .
+        self.goal_pos=goal_pos #tuple of the position(s) of .
         self.curr_ok=curr_ok #number of $ at the spot .
 
 class Sokoban(Problem):
     def __init__(self, initial):
         #todo : ouverture des deux fichiers pour remplir grid et curr_pos
         self.initial = State(grid, curr_pos, goal_pos, 0) #note on peut calculer le nb de curr_ok selon le grid initial mais dans aucun des cas il y a un ok dès le début
+    
+    def goal_test(self, state):
+        return state.curr_ok == len(state.goal_pos)
+    
     def actions(self, state):
         directions= [[1, 0], [-1, 0], [0, -1], [0, 1]]
+        direction_checked=[[]]
 
         for direction in directions:
             direction_checked[0] = state.curr_pos[0] + direction [0]
@@ -26,9 +31,10 @@ class Sokoban(Problem):
                 directions.remove(direction)
 
             if state.grid[direction_checked[0]][direction_checked[1]] == "$": #todo: ajouter un indicateur pour communiquer à result qu'une boite à été déplacée -> ajouter une lettre 
+                side_checked=[[]]
                 for side in directions: #check around the box
-                    side_checked[0]=pos_checked[0]+side[0]
-                    side_checked[1]=pos_checked[1]+side[1]
+                    side_checked[0]=direction_checked[0]+side[0]
+                    side_checked[1]=direction_checked[1]+side[1]
 
                     if (state.grid[side_checked[0]][direction_checked[1]] == "#" or "$") and (side == direction): #si une boite est présente ou un mur autour de la boite initial, vérifie que ca soit dans la meme direction, et annule l'action auquel cas
                         directions.remove(direction)
@@ -47,12 +53,18 @@ class Sokoban(Problem):
 
         if state.grid[new_state.curr_pos[0]][new_state.curr_pos[1]] == "$": #si il y avait une caisse là où le personnage se déplace
             new_state.grid[new_state.curr_pos[0]+action[0]][new_state.curr_pos[1]+action[1]] = "$" #immite le déplacement de la caisse en allant écrire $ un déplacent plus loin dans la direction de action
-        for goal_pos in state.goal_pos:
-            if new_state.grid[goal_pos[0]][goal_pos[1]]=="$": #vérifier si une des position de fin correspond à un $  -> attention il va falloir gérer le cas ou la boite bouge car faut considérer l'état avant
-                new_state.curr_ok+=1    #on augmente le nombre de boite trouvées -> à chier car pose problème lorsqu'on sort une caisse d'une position win, peut être associer un goal pos avec un 1 ou 0 dans le meme ordre ?
-            elif new_state.curr_ok
-
-        return State(new_state.grid, new_state.curr_pos, state.goal_pos)
         
-    def goal_test(self, state):
-        #todo
+        
+        #boucle de calcul de boite à leur place -> TODO faire une fonction
+        new_state.curr_ok=0
+        for goal_pos in state.goal_pos:
+            if new_state.grid[goal_pos[0]][goal_pos[1]]=="$": #vérifier si une des position de fin correspond à un $  
+                new_state.curr_ok+=1    #on augmente le nombre de boite trouvées
+
+        return State(new_state.grid, new_state.curr_pos, state.goal_pos, new_state.curr_ok)
+        
+
+
+######################
+# Auxiliary function #
+######################
